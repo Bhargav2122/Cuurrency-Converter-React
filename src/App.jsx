71c564a52f0578512d2e484b26c014a365e68f4d
balabhargav21 <140328274+balabhargav21@users.noreply.github.com>
2024-11-20 19:27:01 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ const App = () => {
        backgroundImage:
          "url('https://images.unsplash.com/photo-1526304640581-d334cdbbf45e?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D')",
      }}>
-       <div className="bg-[rgba(2,7,40,0.5)] p-10 rounded-[25px] backdrop-blur-[10px] border-2 border-[#cdcdcd] sm:w-[350px] sm:p-7">
-          <h1 className="m-2 text-[2rem] xs:text-[1.75rem]">Currency Converter</h1>
+       <div className="bg-[rgba(2,7,40,0.5)] p-10 rounded-[25px] backdrop-blur-[10px] border-2 border-[#cdcdcd]  max-w-[380px] sm:w-[100%] sm:p-6">
+          <h1 className="m-2 text-[2rem] xs:text-[1.5rem]">Currency Converter</h1>
           <Converter
           amount={amount}
           setAmount={setAmount}
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -8,7 +8,7 @@ const Select = (props) => {
   } = props
   return (
     <>
-       <select className="bg-[rgba(2,7,40,0.5)] w-[85%] h-[5.8vh] text-[1rem] p-[3px_5px]"  value={selectedCurrency} onChange={onChangeCurrency}>
+       <select className="bg-[rgba(2,7,40,0.5)] w-[80%] h-[5.8vh] text-[1rem] p-[3px_5px]"  value={selectedCurrency} onChange={onChangeCurrency}>
         {currencyOptions.map((option) => (
            <option className="bg-[#030728]"  key={option} value= {option}>{option}</option>
         ))}
